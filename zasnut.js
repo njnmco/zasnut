9a3d72e2ae9838759d387150b97c6f1cb50aae5e
Neal Fultz <nfultz@gmail.com>
2021-01-30 18:07:55 +0000

    refactor

--- zasnut.js
+++ zasnut.js
@@ -1,26 +1,19 @@
 
-(function(){
-    // windowId => {windowId, tabId} of active tab
-    var activeMap = new Map();
-
-    // Configurable delay
-    var zasnut = { delayInMinutes: 4 }
-    //chrome.storage.local.get("zasnut", o => Object.assign(zasnut, o))
-    //chrome.storage.local.onChanged.addListener( o => o.zasnut && Object.assign(zasnut, o.zasnut.newValue))
-
-    chrome.tabs.onActivated.addListener(function(active) {
-      let repr = (info) =>`zzz_${info.tabId}`;
-      chrome.alarms.clear(repr(active));
-      let last = activeMap.get(active.windowId);
-      if (last) chrome.alarms.create(repr(last), zasnut);
-      activeMap.set(active.windowId, active);
-    })
-})();
+chrome.tabs.onActivated.addListener(function(active) {
+    chrome.alarms.create(
+        active.tabId.toString(),
+        { periodInMinutes: 2 }
+    );
+})
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
-  let check = (t) => t && !t.active && !t.audible && !t.pinned && t.autoDiscardable;
-  if (!alarm.name.startsWith("zzz_")) return;
-  let tabId = parseInt(alarm.name.substr(4));
-  chrome.tabs.get(tabId, (t) => chrome.runtime.lastError || check(t) && chrome.tabs.discard(t.id))
+    chrome.tabs.get(parseInt(alarm.name), (t) => {
+        if (!chrome.runtime.lastError && t) {
+            if (t.active || t.audible || t.pinned || !t.autoDiscardable)
+                return true;
+            chrome.tabs.discard(t.id);
+        }
+        chrome.alarms.clear(alarm.name);
+    })
 })
   
