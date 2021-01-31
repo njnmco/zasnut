6d924346050644309dbb127ace65442be102c892
Neal Fultz <nfultz@gmail.com>
2021-01-31 23:24:15 +0000

    remove listener

--- zasnut.js
+++ zasnut.js
@@ -1,19 +1,16 @@
 
 chrome.tabs.onActivated.addListener(function(active) {
-    chrome.alarms.create(
-        active.tabId.toString(),
-        { periodInMinutes: 2 }
-    );
+    chrome.alarms.create(active.tabId.toString(), {periodInMinutes: 2});
 })
+
+chrome.tabs.onRemoved.addListener((tId) => chrome.alarms.clear(tId.toString()));
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     chrome.tabs.get(parseInt(alarm.name), (t) => {
-        if (!chrome.runtime.lastError && t) {
-            if (t.active || t.audible || t.pinned || !t.autoDiscardable)
-                return true;
-            chrome.tabs.discard(t.id);
-        }
-        chrome.alarms.clear(alarm.name);
+        if (t.active || t.audible || t.pinned || !t.autoDiscardable)
+            return true;
+        chrome.tabs.discard(t.id, (t) => chrome.alarms.clear(alarm.name));
     })
 })
+
   
